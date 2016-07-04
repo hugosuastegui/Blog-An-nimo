e96f121ee38512dfc0b53f46c521840b38181829
codeamac07 <codeamac07@gmail.com>
2016-07-04 17:38:31 -0500

    Todos las rutas de post funcionando correctamente

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,34 +4,18 @@ get '/' do
   erb :index
 end
 
-post '/write_post' do 
-  a = params[:title]
-  b = params[:content]
-  c = params[:tags]
-  post = Post.create(title: a, description: b)
-  c_array = c.split(",")
-  c_array.each do |c|
-    post.tags << Tag.create(name: c)
-  end 
-  erb :index
-end
 
-get '/show_all_posts' do 
-  
-end
 
-get '/show_a_post' do 
-  
-end
 
-get '/show_all_posts' do 
-  
-end
 
-get '/edit_a_post' do 
-  
-end
 
 get '/delete_a_post' do 
-
+  id = params[:id]
+  unless Post.where(id: id).empty?
+    @post = Post.find(id)
+    @post.destroy
+  else
+    @post = 1
+  end
+  erb :deleted_post
 end
